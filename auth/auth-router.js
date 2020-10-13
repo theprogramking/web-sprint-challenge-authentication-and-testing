10a0693848b92298e718a8d4dd025e4067d53af1
Luke Shimkus <lukeshimkus@gmail.com>
2020-10-12 22:40:40 -0400

    Init.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,59 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require("../user/user-model");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/secrets");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 20);
+  user.password = hash;
+  Users.add(user)
+    .then((savedUser) => {
+      res.status(200).json(savedUser);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        err: error,
+        message: "There was an error registering user.",
+      });
+    });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: "Howdy",
+          token,
+        });
+      } else {
+        res.status(401).json({
+          message: "Invalid creds bro.",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "There was an error logging in.",
+        err: error,
+      });
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "1h",
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,25 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/secrets");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { authorization } = req.headers;
+  if (authorization) {
+    jwt.verify(authorization, jwtSecret, (error, token) => {
+      if (err) {
+        res.status(401).json({
+          message: "Invalid creds man!",
+        });
+      } else {
+        req.token = token;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({
+      message: "No creds",
+    });
+  }
 };
